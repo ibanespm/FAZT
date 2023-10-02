4245f64f6cec1b239b0378b17eb24aa00d8816d3
fazt <ibanes59@gmail.com>
2023-10-01 19:19:25 -0500

    estamos comentando algunos proyectos

---
+++ python.py
@@ -0,0 +1,20 @@
+
+
+
+class Persona:
+    def __init__(self,firstName, lastName, year, codig):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.year = year
+        self.codig =   codig
+    def getName(self):
+        return self.getName + self.lastName
+    
+
+
+class TargetaCredito:3
+
+
+
+
+   
